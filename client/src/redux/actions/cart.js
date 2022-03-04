a9c23b03b3ac4062a5daff86474436e07f9ff058
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-03-04 18:09:35 +0100

    Added reducers to handle cart items

--- client/src/redux/actions/cart.js
+++ client/src/redux/actions/cart.js
@@ -1,3 +1,18 @@
-import { ADD_ITEM } from "../../constants/actionTypes";
+import {
+  ADD_ITEM,
+  DECREASE_QTY,
+  DELETE_ITEM,
+  INCREASE_QTY,
+  UPDATE_TOTAL,
+} from "../../constants/actionTypes";
 
-export const addItemAction = (product) => ({ type: ADD_ITEM, payload: product });
+export const addItemAction = (product) => ({
+  type: ADD_ITEM,
+  payload: product,
+});
+
+export const updateTotalAction = () => ({ type: UPDATE_TOTAL });
+
+export const increaseQtyAction = (id) => ({ type: INCREASE_QTY, payload: id });
+export const decreaseQtyAction = (id) => ({ type: DECREASE_QTY, payload: id });
+export const deleteItemAction = (deleteItem) => ({ type: DELETE_ITEM, payload: deleteItem });
